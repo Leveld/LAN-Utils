05d5f3fb305149948f2e2379ed156a080fbc64b3
Jourdan Clark <jourdan_clark@hotmail.com>
2018-02-12 18:43:15 -0700

    Removed test code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-const { logger } = require('./util');
-
-logger('hello');
-logger.log('hello');
-logger.info('hello');
-logger.warn('hello');
-logger.error('hello');
-
-
 module.exports = {
   ...require('./util')
 };
